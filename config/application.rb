811cea762cbadfac32f2f8f36862a137bccc084e
EC2 Default User <ec2-user@ip-172-31-3-108.us-east-2.compute.internal>
2021-03-29 02:45:48 +0000

    slim-railsの導入

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,12 @@ module Insta
       g.test_framework false #テストを生成しない
     end
     config.time_zone = 'Tokyo' #タイムゾーンを設定
+    
+    module App
+      class Application < Rails::Application
+        config.generators.template_engine = :slim #slimに変更
+      end                                                                                                                                                                     
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
